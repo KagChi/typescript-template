3cc110d4faf9fca363cdb9235c715233981a78d9
Samuel <yonuxbot@gmail.com>
2025-09-25 14:01:47 +0000

    fix: correctly setup biome

--- eslint.config.js
+++
@@ -1,3 +0,0 @@
-import { common, modules, node, stylistic, typescript, ignores } from "@hazmi35/eslint-config";
-
-export default [...common, ...modules, ...node, ...stylistic, ...typescript, ...ignores];
